eeae786e62343ea838f72a2c0b53abe8cea956a0
nolan <nolan@e-major.fr>
2021-06-10 15:39:24 +0200

    init 3 page

---
+++ lib/main.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Firebase'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        backgroundColor: Colors.black,
+      ),
+      body: body(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Menu'),
+            ),
+            ListTile(
+              title: Text('Profil'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Profil()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget body() {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        children: [
+          Center(
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Pseudo'),
+            ),
+          ),
+          Center(
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Mot de passe'),
+            ),
+          ),
+          Center(
+              child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Inscription()),
+                    );
+                  },
+                  child: Text('S\'inscrire'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  child: Text('Connexion'),
+                ),
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class Inscription extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inscription"),
+        backgroundColor: Colors.black,
+      ),
+      body: body(),
+    );
+  }
+  Widget body() {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        children: [
+          Center(
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Pseudo'),
+            ),
+          ),
+          Center(
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Mot de passe'),
+            ),
+          ),
+          Center(
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Mail'),
+            ),
+          ),
+          Center(
+              child: Container(
+                child: ElevatedButton(
+                      onPressed: () {
+                      },
+                      child: Text('S\'inscrire'),
+                    ),
+                ),
+              ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class Profil extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profil"),
+        backgroundColor: Colors.black,
+      ),
+      body: body(),
+    );
+  }
+  Widget body() {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        children: [
+          Center(
+            child: Text('Mon profil')
+          ),
+        ],
+      ),
+    );
+  }
+}
